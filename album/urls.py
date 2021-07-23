c228c585c8f0dbaf1fb41a478df951e3d4a17bf8
Dusai <dusaiphoto@foxmail.com>
2021-07-23 14:22:01 +0800

    u

--- album/urls.py
+++ album/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('photo/', include('photo.urls', namespace='photo')),
     path('', home, name='home'),
-]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
