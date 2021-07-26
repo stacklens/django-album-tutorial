720572bb6e25a1f96c30c3866158f3590c398286
Dusai <dusaiphoto@foxmail.com>
2021-07-26 15:59:45 +0800

    u

--- photo/views.py
+++ photo/views.py
@@ -6,14 +6,10 @@ from django.core.paginator import Paginator
 
 def home(request):
     photos = Photo.objects.all()
-
     paginator    = Paginator(photos, 5)
     page_number  = request.GET.get('page')
     paged_photos = paginator.get_page(page_number)
-
     context = {'photos': paged_photos}
-
-
 
     # 处理登入登出的POST请求
     if request.method == 'POST':
